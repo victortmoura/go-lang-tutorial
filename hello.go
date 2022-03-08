5c57488f47b56828b30be339f0d358ce20e51293
Victor Moura <jose.moura@bancointer.com.br>
2022-03-08 18:59:48 -0300

    Desenv. requisicoes web

--- hello.go
+++ hello.go
@@ -2,11 +2,45 @@ package main
 
 import (
 	"fmt"
-	"reflect"
+	"net/http"
+	"os"
 )
 
 func main() {
 
+	exibirIntroducao()
+
+	exibirMenu()
+
+	comando := lerComando()
+
+	switch comando {
+	case 1:
+		fmt.Println("Monitorando...")
+		fmt.Println(monitorar())
+	case 2:
+		fmt.Println("Exibindo Logs...")
+	case 0:
+		fmt.Println("Saindo do Programa...")
+		os.Exit(0)
+	default:
+		fmt.Println("Comando ", comando, "inexistente")
+		os.Exit(-1)
+	}
+
+	// Outro modo de realizar a validacao do input do usuario
+	// if comando == 1 {
+	// 	fmt.Println("Monitorando...")
+	// } else if comando == 2 {
+	// 	fmt.Println("Exibindo Logs...")
+	// } else if comando == 0 {
+	// 	fmt.Println("Saindo do Programa")
+	// } else {
+	// 	fmt.Println("Comando ", comando, "inexistente")
+	// }
+}
+
+func exibirIntroducao() {
 	// definindo tipo da variavel
 	// var nome string = "Victor Moura"
 	// var nome = "Victor Moura"
@@ -17,19 +51,29 @@ func main() {
 
 	fmt.Println("Prazer", nome)
 	fmt.Println("Você está na versão:", versao)
-	fmt.Println("* Tipo da variável versão é:", reflect.TypeOf(versao))
 	fmt.Println()
+}
 
+func exibirMenu() {
 	fmt.Println("1- Iniciar Monitoramento")
 	fmt.Println("2- Exibir Logs")
 	fmt.Println("0- Sair do Programa")
+}
 
-	var comando int
+func lerComando() int {
+	var comandoLido int
 	// ao utilizar a funcao ScanF, deve-se inferir o modificador %d (que representa digito)
 	// fmt.Scanf("%d", comando)
 
 	// Lembrar de colocar o '&' pois o input ira ser salvo no endereco de memoria da variavel
-	fmt.Scan(&comando)
+	fmt.Scan(&comandoLido)
 
-	fmt.Println("O comando selecionado foi", comando)
+	return comandoLido
+}
+
+func monitorar() string {
+	url := "https://www.alura.com.br"
+	resp, _ := http.Get(url)
+
+	return resp.Status
 }
